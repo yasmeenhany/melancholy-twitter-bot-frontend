92aa7e5e8cbdd07e5d9741d012c14f7070f86c94
yasmeen <yasmeen.abdelhafiz@gmail.com>
2020-05-11 22:20:14 +0200

    fixed time bug

--- script.py
+++ script.py
@@ -8,19 +8,17 @@ logger = logging.getLogger()
 
 def main():
     now = datetime.datetime.now().time()
-    print(now.hour == 22 and now.minute==7)
+    print((now.hour == 22 and now.minute == 17) or (now.hour == 4 and now.minute == 0))
     auth = tweepy.OAuthHandler(os.environ['TWITTER_CONSUMER_KEY'], os.environ['TWITTER_CONSUMER_SECRET'])
     auth.set_access_token(os.environ['ACCESS_KEY'], os.environ['ACCESS_SECRET'])
 
     # Create API object
     api = tweepy.API(auth)
-    since_id = 2
     while True:
         now = datetime.datetime.now().time()
-        if (now.hour == 22 and now.minute == 15) or (now.hour == 4 and now.minute == 0):
-            api.update_status("Hi " + now)
-            since_id = since_id+1
-            logger.info("Waiting...")
+        if (now.hour == 22 and now.minute == 25) or (now.hour == 16 and now.minute == 0):
+            api.update_status("hi from " + str(now))
+        print(now.hour + "," + now.minute)
 
 if __name__ == "__main__":
     main()
